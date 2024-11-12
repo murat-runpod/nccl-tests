5833b335d492dfaeee2d8b488c39b8da7b72e6ea
Murat Magomedov <muratmagomedov@Murats-MacBook-Pro.local>
2024-11-12 11:50:35 -0800

    scripts

---
+++ runslave.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export NCCL_DEBUG=INFO
+export NCCL_SOCKET_IFNAME=bond0.2411
+./torch/bin/torchrun \
+    --nproc_per_node=1 \
+    --nnodes=2 \
+    --node_rank=1 \
+    --rdzv_id=687 \
+    --rdzv_backend=static \
+    --rdzv_endpoint=213.173.102.185:55000 \
+   main.py
