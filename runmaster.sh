f72aecebe54d7ef068ff3c9f02fce106ad2b9446
Murat Magomedov <muratmagomedov@Murats-MacBook-Pro.local>
2024-11-26 13:45:46 -0800

    updated ip addresses and interfaces

--- runmaster.sh
+++ runmaster.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-export NCC_DEBUG=INFO
-export NCCL_SOCKET_IFNAME=bond0.2411
+export NCCL_DEBUG=INFO
+#get default interface from 'ip route'
+export NCCL_SOCKET_IFNAME=bond0.2636
 ./torch/bin/torchrun \
     --nproc_per_node=1 \
     --nnodes=2 \
     --node_rank=0 \
     --rdzv_id=687 \
     --rdzv_backend=static \
-    --rdzv_endpoint=213.173.102.185:55000 \
+    --rdzv_endpoint=213.173.111.94:55000 \
    main.py
--- runslave.sh
+++ runslave.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 export NCCL_DEBUG=INFO
-export NCCL_SOCKET_IFNAME=bond0.2411
+#get default interface from 'ip route'
+export NCCL_SOCKET_IFNAME=bond0.2636
 ./torch/bin/torchrun \
     --nproc_per_node=1 \
     --nnodes=2 \
     --node_rank=1 \
     --rdzv_id=687 \
     --rdzv_backend=static \
-    --rdzv_endpoint=213.173.102.185:55000 \
+    --rdzv_endpoint=213.173.111.94:55000 \
    main.py
