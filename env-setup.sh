b1d3ff033ff88ab2d8199ea4a210739f66899e39
Murat Magomedov <muratmagomedov@Murats-MacBook-Pro.local>
2024-11-27 13:39:34 -0800

    torch env

--- env-setup.sh
+++ env-setup.sh
@@ -5,6 +5,6 @@ apt install python3-venv -y
 apt install python3-pip -y
 
 python3 -m venv .torch
-./.torch/bin/activate \
-    python3 -m pip install torch torchvision torchaudio
+source ./.torch/bin/activate
+python3 -m pip install torch torchvision torchaudio
 
