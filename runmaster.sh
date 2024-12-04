d0a749001c01610837365b66be42851467ef53fd
Murat Magomedov <muratmagomedov@Murats-MacBook-Pro.local>
2024-12-04 13:44:29 -0800

    test

--- runmaster.sh
+++ runmaster.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 export NCCL_DEBUG=INFO
 #get default interface from 'ip route'
-export NCCL_SOCKET_IFNAME=bond0,bond0.2636,bond0.3118
-export NCCL_NSOCS_PERTHREAD=16
-export NCCL_SOCKET_NTHREADS=16
+export NCCL_SOCKET_IFNAME=bond0.2636
+#export NCCL_NSOCS_PERTHREAD=16
+#export NCCL_SOCKET_NTHREADS=16
 ./.torch/bin/torchrun \
     --nproc_per_node=1 \
     --nnodes=2 \
