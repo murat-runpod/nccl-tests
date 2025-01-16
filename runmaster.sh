feb55990239f05d866fcda7df8c50ab140ba253e
Murat Magomedov <muratmagomedov@Murats-MacBook-Pro.local>
2025-01-16 12:25:26 -0800

    use env vars

--- runmaster.sh
+++ runmaster.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 export NCCL_DEBUG=INFO
 #get default interface from 'ip route'
-export NCCL_SOCKET_IFNAME=bond0.2636
+export NCCL_SOCKET_IFNAME=eth1
 #export NCCL_NSOCS_PERTHREAD=16
 #export NCCL_SOCKET_NTHREADS=16
-export NCCL_MIN_NCHANNELS=2
-./.torch/bin/torchrun \
+#export NCCL_MIN_NCHANNELS=2
+torchrun \
     --nproc_per_node=1 \
     --nnodes=2 \
     --node_rank=0 \
     --rdzv_id=687 \
     --rdzv_backend=static \
-    --rdzv_endpoint=213.173.111.94:55000 \
+    --rdzv_endpoint=$MASTER_ADDR:55000 \
    main.py
    
