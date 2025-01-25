9bf870d542342b993d2f1eda9e0cc648a956f7b7
Murat Magomedov <muratmagomedov@Murats-MacBook-Pro.local>
2025-01-24 16:15:20 -0800

    more detailed NCCL benchmarking

--- runmaster.sh
+++ runmaster.sh
@@ -2,8 +2,8 @@
 export NCCL_DEBUG=INFO
 #get default interface from 'ip route'
 export NCCL_SOCKET_IFNAME=eth1
-#export NCCL_NSOCS_PERTHREAD=16
-#export NCCL_SOCKET_NTHREADS=16
+export NCCL_NSOCS_PERTHREAD=4
+export NCCL_SOCKET_NTHREADS=4
 #export NCCL_MIN_NCHANNELS=2
 torchrun \
     --nproc_per_node=1 \
@@ -12,5 +12,9 @@ torchrun \
     --rdzv_id=687 \
     --rdzv_backend=static \
     --rdzv_endpoint=$MASTER_ADDR:55000 \
-   main.py
+   main.py \
+   --size 1000000 \
+   --iterations 20 \
+   --warmup 5 \
+   --profile
    
