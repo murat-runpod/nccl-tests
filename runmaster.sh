839c09cc7ec2f4e5953ca7ca0bc7490f83d2c995
Murat Magomedov <muratmagomedov@Murats-MacBook-Pro.local>
2025-01-29 14:41:16 -0800

    nccl channels = 2

--- runmaster.sh
+++ runmaster.sh
@@ -4,7 +4,7 @@ export NCCL_DEBUG=INFO
 export NCCL_SOCKET_IFNAME=eth1
 export NCCL_NSOCS_PERTHREAD=4
 export NCCL_SOCKET_NTHREADS=4
-#export NCCL_MIN_NCHANNELS=2
+export NCCL_MIN_NCHANNELS=2
 torchrun \
     --nproc_per_node=1 \
     --nnodes=2 \
