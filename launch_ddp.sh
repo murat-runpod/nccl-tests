1c7fae70abf8e3c2b57c52500d563a41c9bdf6d9
Murat Magomedov <muratmagomedov@Murats-MacBook-Pro.local>
2025-02-03 14:07:05 -0800

    env var ddp launcher

---
+++ launch_ddp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python3 -m torch.distributed.launch \
+    --nproc_per_node=$NUM_TRAINERS \
+    --nnodes=$NUM_NODES \
+    --node_rank=$NODE_RANK \
+    --master_addr=$MASTER_ADDR \
+    --master_port=$MASTER_PORT \
+    main.py
--- runworker.sh
+++ runworker.sh
@@ -2,7 +2,7 @@
 torchrun \
     --nproc_per_node=$NUM_TRAINERS \
     --nnodes=2 \
-    --node_rank=0 \
+    --node_rank=1 \
     --master_addr=$MASTER_ADDR \
     --master_port=55000 \
    main.py
