b9082ace51c64aff911736739fdf10e51f14b730
Murat Magomedov <muratmagomedov@Murats-MacBook-Pro.local>
2025-02-06 13:17:01 -0800

    correct local rank

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from datetime import timedelta
 
 def ddp_setup():
-    torch.cuda.set_device(int(os.environ["LOCAL_RANK"]))
+    local_rank = int(os.environ["LOCAL_RANK"])
+    torch.cuda.set_device(local_rank)  # Set device before init_process_group
     dist.init_process_group(backend="nccl", init_method="env://", timeout=timedelta(seconds=10))
 
 def ddp_cleanup():
@@ -16,9 +17,12 @@ def ddp_cleanup():
 if __name__ == "__main__":
     ddp_setup()
     rank = int(os.environ["RANK"])
-    device = torch.device("cuda:0")
-    print("global rank = " + str(rank))
-    print("device = " + torch.cuda.get_device_name(device))
+    local_rank = int(os.environ["LOCAL_RANK"])
+    device = torch.device(f"cuda:{local_rank}")  # Use local_rank for device assignment
+    
+    print(f"global rank = {rank}")
+    print(f"local rank = {local_rank}")
+    print(f"device = {torch.cuda.get_device_name(device)}")
 
     tensor = torch.ones(1 * 10**9, device=device)
     dist.all_reduce(tensor, op=dist.ReduceOp.SUM)
@@ -27,7 +31,7 @@ if __name__ == "__main__":
         tensor = torch.ones(5 * 10**9, device=device)
         dist.all_reduce(tensor, op=dist.ReduceOp.SUM)
     
-    print("Size in memory (Mb): " + str(tensor.element_size() * tensor.nelement() / 10**6))
+    print(f"Size in memory (Mb): {tensor.element_size() * tensor.nelement() / 10**6}")
     print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
     
     ddp_cleanup()
